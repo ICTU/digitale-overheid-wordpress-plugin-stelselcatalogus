c55a98ab62d39eec2d1f8c4b914842cad1375ac8
PR van Buuren <paul@appelflap.fritz.box>
2016-11-29 18:10:56 +0100

    TEmplate
    Oplevering. Dossierlink toegevoegd.

--- rhswp-stelselcatalogus.php
+++ rhswp-stelselcatalogus.php
@@ -6,7 +6,7 @@
  * Plugin URI:    https://wbvb.nl/plugins/rhswp-stelselcatalogus/
  * Description:   De mogelijkheid om een stelselplaat te tonen op een pagina
  * Version:       1.0.1
- * Version desc:  Volgende slag: pijlenschema opgeschoond. 
+ * Version desc:  Oplevering. Dossierlink toegevoegd. 
  * Author:        Paul van Buuren
  * Author URI:    https://wbvb.nl
  * License:       GPL-2.0+
